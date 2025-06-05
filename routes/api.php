597fbe4c003f97c0da6396a9bd822e57ac528f58
augustomauro <amtnet@hotmail.com>
2025-06-04 22:20:46 -0300

    update sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/user', function (Request $request) {
+//     return json_encode($request);
+// });
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
